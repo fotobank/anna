dab7463b35ec7fe5d8d80222d345cd604cc334e3
Алексеев Юрий <aleksjurii@gmail.com>
2015-04-14 15:09:07 +0300

    14.04.15

--- inc/wm.php
+++ inc/wm.php
@@ -16,7 +16,7 @@ if ( $path && preg_match( '#\.(gif|jpeg|jpg|png)$#i', $path ) ) {
 
 
 	$path = WinUtf( $path, 'u' );
-	$path = urlencode($path);
+//	$path = urlencode($path);
 
 
 	$realpath = $_SERVER['DOCUMENT_ROOT'] . '/' . $portolio_dir . $path;
