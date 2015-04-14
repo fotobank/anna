66de0ae4ff4facff890cc632e105f4603f222c54
Алексеев Юрий <aleksjurii@gmail.com>
2015-04-14 21:11:54 +0300

    14.04.15

--- inc/th.php
+++ inc/th.php
@@ -16,7 +16,7 @@ require_once (__DIR__ . "/../inc/func.php");
 	$portolio_dir = "files/portfolio/";
 
 
-	/*if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) {
+	if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) {
 
 		$path = iconv( 'windows-1251', 'utf-8', $path );
 		// basename с поддержкой cp1251 для utf-8
@@ -26,13 +26,16 @@ require_once (__DIR__ . "/../inc/func.php");
 
 		$file_name = basename($path);
 	}
-	$realpath = $_SERVER['DOCUMENT_ROOT'] . '/' . $portolio_dir . dirname($path) . "/thumb/" . $file_name;*/
+	$realpath = $_SERVER['DOCUMENT_ROOT'] . '/' . $portolio_dir . dirname($path) . "/thumb/" . $file_name;
 
 
 
-	if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) $path = iconv( 'windows-1251', 'utf-8', $path );
+//	if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) $path = iconv( 'windows-1251', 'utf-8', $path );
 
-	$realpath = $_SERVER['DOCUMENT_ROOT'] . '/' . $portolio_dir . dirname($path) . "/thumb/" . __basename($path);
+//	$realpath = $_SERVER['DOCUMENT_ROOT'] . '/' . $portolio_dir . dirname($path) . "/thumb/" . __basename($path);
+
+
+
 	$image    = @imagecreatefromstring( @file_get_contents( $realpath ) );
 	if ( ! $image ) {
 		error_log( "\$realpath = " . $realpath . " \$image = " . $image, 0 );
