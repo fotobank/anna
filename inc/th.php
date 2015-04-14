52af3a3fe7ba56861d934faea4d2607a92973107
Алексеев Юрий <aleksjurii@gmail.com>
2015-04-14 16:02:43 +0300

    14.04.15

--- inc/th.php
+++ inc/th.php
@@ -15,8 +15,9 @@ if ( $path && preg_match( '#\.(gif|jpeg|jpg|png)$#i', $path ) ) {
 	$portolio_dir = "files/portfolio/";
 
 
+	$path = urlencode($path);
 
-//	if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) $path = iconv( 'windows-1251', 'utf-8', $path );
+	if ( $_SERVER['REMOTE_ADDR'] !== '127.0.0.1' ) $path = iconv( 'windows-1251', 'utf-8', $path );
 
 
 
