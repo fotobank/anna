d8723240e71bffa7c64ecf4329a6cd86e848e168
Алексеев Юрий <aleksjurii@gmail.com>
2015-05-25 16:47:01 +0300

    25.05.15

--- inc/func.php
+++ inc/func.php
@@ -1777,3 +1777,119 @@ function user_phone()
 
 		return $phone;
 	}
+
+
+/**
+ * @param $file_name
+ *
+ * Обработка фотографий сделанных с мобильных устройств
+ * проверка фоток на то, что они сделаны с мобильного устройства
+ * проверяем ориентацию фотки и если нужно вернем градус, на который нужно повернуть фото
+ *
+ * @return array
+ */
+function getSizePhotoMobile($file_name){
+	// получаем EXIF-заголовки
+	$exif_read_data = @exif_read_data($file_name);
+	$size = @getimagesize($file_name);
+	$width = $size[0];
+	$height = $size[1];
+	$degree = 0;
+	// если заголовки получили, и среди них нашлось упоминание об ориентации
+	if($exif_read_data){
+		if(isset($exif_read_data["Orientation"]) AND $exif_read_data["Orientation"] > 4){
+			$size = getimagesize($file_name, $info);
+			$width = $size[1];
+			$height = $size[0];
+			switch ($exif_read_data["Orientation"]){
+				case 5:
+					$degree = 270;
+					break;
+				case 6:
+					$degree = 270;
+					break;
+				case 7:
+					$degree = 90;
+					break;
+				case 8:
+					$degree = 90;
+					break;
+			}
+		}
+	}
+	return [
+		0 => $width,
+		1 => $height,
+		'degree' => $degree
+	];
+}
+
+/**
+ * @param $img
+ * @param $degree
+ *
+ * @return bool|resource
+ *
+ * узнаем размеры исходной картинки
+ * $size = getSizePhotoMobile($source);
+ * если это фото сделано с мобильного телефона
+ * if($size['degree'] > 0) {
+ * $photo = rotatePhotoMobile($source, $size['degree']);
+ * } else {
+ * код для обычных фоток
+ * }
+ *
+ */
+function rotatePhotoMobile($img, $degree){
+	// получаем данные о картинке
+	$size = getimagesize($img);
+	//определяем тип (расширение) картинки
+	$format = strtolower(substr($size['mime'], strpos($size['mime'], '/')+1));
+	$icfunc = "imagecreatefrom" . $format;   //определение функции для расширения файла
+	//если нет такой функции, то прекращаем работу скрипта
+	if (!function_exists($icfunc)) return false;
+	// Загрузка изображения
+	$source = $icfunc($img);
+	// Поворот. Пустые углы заливаем цветом 0xffffff
+	$rotate = imagerotate($source, $degree, '0xd72630');
+	return $rotate;
+}
+
+/**
+ * Определяем на чем работает сайт
+ *
+ * @param $url - string URL сайта
+ *
+ * @return string результат проверки
+ *
+ * $url = 'http://vk-book.ru';
+ * $result = getPoweredBy($url);
+ * print_r ($result);
+ *
+ */
+function getPoweredBy($url){
+	$tmp = parse_url($url);
+	$stream = @fopen($url, 'rb'); // открываем сайт
+	if(!$stream){
+		return "Сайт не отвечает!";
+	}
+	$array = stream_get_meta_data($stream); // получаем заголовки
+	$info = false;
+	// находим информацию о X-Powered-By
+	foreach($array["wrapper_data"] as $k=>$v){
+		if(strpos($v, 'X-Powered-By:') !== false){
+			$info = explode('X-Powered-By:', $v);
+		}
+	}
+	// вернем результат
+	if($info){
+		$powered_by = trim($info[1]);
+		return $powered_by;
+	}else{
+		return "Не известно!";
+	}
+}
+
+$url = 'http://vk-book.ru';
+$result = getPoweredBy($url);
+print_r ($result);
