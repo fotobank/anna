f2aa72275148aebed7f18124f4e321143c21f02d
Алексеев Юрий <aleksjurii@gmail.com>
2015-05-30 02:21:27 +0300

    29.05.15

--- system/classes/Router/Router.php
+++ system/classes/Router/Router.php
@@ -65,7 +65,7 @@ Class Router
 			// Находим контроллер
 			$cmd_path = $this->path;
 			foreach ($parts as $part) {
-				$part = ucfirst($part);
+
 				$fullpath = $cmd_path . $part;
 
 				// Проверка существования папки
