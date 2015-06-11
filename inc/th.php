da210c8caa5eec82b466d7ad6a7ecf4ff98d52c9
Алексеев Юрий <aleksjurii@gmail.com>
2015-06-12 01:14:18 +0300

    12.06.15

--- inc/th.php
+++ inc/th.php
@@ -13,21 +13,21 @@ if (!defined('CODE_PAGE')) {
 	define( 'CODE_PAGE', detect_encoding(SITE_PATH . 'inc/кодировка файловой системы.codepage'));
 }
 
-$path = isset( $_GET['img'] ) ? $_GET['img'] : false;
-$path = parse_url($path, PHP_URL_PATH);
-
-if ( CODE_PAGE == 'utf-8' ) {
-	$path = cp1251_utf8($path);
-} elseif ( CODE_PAGE == 'windows-1251' ) {
-	$path = utf8_cp1251($path);
-}
+$path = isset( $_GET['img'] ) ? parse_url($_GET['img'], PHP_URL_PATH) : false;
+$realpath = SITE_PATH . 'files/portfolio/';
 
 if ( $path && preg_match( '#\.(gif|jpeg|jpg|png)#i', $path ) ) {
 
-	$dirname = $basename = '';
-	extract(path_info( $path, EXTR_OVERWRITE )); // если переменная существует она будет переписана
-	$realpath = SITE_PATH . 'files/portfolio/' . $dirname . "/thumb/" . $basename;
+	if (CODE_PAGE == 'utf-8') {
+		$path = cp1251_utf8($path);
+	} elseif (CODE_PAGE == 'windows-1251') {
+		$path = utf8_cp1251($path);
+	}
 
+	$dirname = $basename = '';
+	extract(path_info($path, EXTR_OVERWRITE));
+	$realpath .= $dirname . "/thumb/" . $basename;
+}
 	$image    = @imagecreatefromstring( @file_get_contents( $realpath ) );
 	if ( ! $image ) {
 		error_log( "\$realpath = " . $realpath . " \$image = " . $image, 0 );
@@ -36,4 +36,3 @@ if ( $path && preg_match( '#\.(gif|jpeg|jpg|png)#i', $path ) ) {
 
 	header( 'Content-type: image/jpeg' );
 	imagejpeg( $image, null, 95 );
-}
