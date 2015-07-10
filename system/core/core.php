8449757e9883920232c7ab80cc3677008b1207b0
Алексеев Юрий <aleksjurii@gmail.com>
2015-07-10 16:35:18 +0300

    11.07.2015

--- system/core/core.php
+++ system/core/core.php
@@ -19,7 +19,7 @@ if (session_id() === '') {
 
 // включить админа для отладки
 if ($_SERVER['REMOTE_ADDR'] === '176.119.96.114') {
-	$_SESSION['logged'] = '0';
+	$_SESSION['logged'] = '1';
 }
 
 if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1' || (array_key_exists('logged', $_SESSION) && $_SESSION['logged'] === '1')) {
